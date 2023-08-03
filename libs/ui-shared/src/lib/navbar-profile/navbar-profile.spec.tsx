f42322cb5ed9c762d7ef3bc19407c11fdff9a675
bsse1009 <mik858692@gmail.com>
2023-08-03 20:15:42 +0600

    added more unit test

--- libs/ui-shared/src/lib/navbar-profile/navbar-profile.spec.tsx
+++ libs/ui-shared/src/lib/navbar-profile/navbar-profile.spec.tsx
@@ -1,5 +1,18 @@
-describe('NavbarProfile', () => {
+import { store } from '@tech-glimpse-front/redux-toolkit';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import NavbarProfile from './navbar-profile';
+
+describe('Navbar with Profile', () => {
   it('should render successfully', () => {
-    expect(true).toBeTruthy();
+    const { baseElement } = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <NavbarProfile />
+        </BrowserRouter>
+      </Provider>
+    );
+    expect(baseElement).toBeTruthy();
   });
 });
--- libs/ui-shared/src/lib/update-user-password-modal/update-user-password-modal.spec.tsx
+++ libs/ui-shared/src/lib/update-user-password-modal/update-user-password-modal.spec.tsx
@@ -1,5 +1,35 @@
-describe('UpdateUserPasswordPage', () => {
+import * as rtkQuery from '@tech-glimpse-front/redux-toolkit';
+import * as ui from '@tech-glimpse-front/ui-shared';
+
+import { render } from '@testing-library/react';
+
+import UpdateUserPasswordModal, {
+  UpdateUserPasswordModalProps,
+} from './update-user-password-modal';
+
+const props: UpdateUserPasswordModalProps = {
+  setShowModal: vi.fn(),
+};
+
+describe('UpdateUserPasswordModal', () => {
+  const useAppSelectorSpy = vi.spyOn(rtkQuery, 'useAppSelector');
+  const useUserSpy = vi.spyOn(rtkQuery, 'useUser');
+  const UpdateUserPasswordFormSpy = vi.spyOn(ui, 'UpdateUserPasswordForm');
+
   it('should render successfully', () => {
-    expect(true).toBeTruthy();
+    useAppSelectorSpy.mockReturnValue({ authUser: 'ibrahim' });
+    useUserSpy.mockReturnValue({
+      updatePasswordByUsername: vi.fn(),
+      updatePasswordLoading: false,
+      updatePasswordError: { message: '' },
+      getUserByUsername: vi.fn(),
+      deleteUserByUsername: vi.fn(),
+    });
+    UpdateUserPasswordFormSpy.mockReturnValue(
+      <div>UpdateUserPasswordForm</div>
+    );
+
+    const { baseElement } = render(<UpdateUserPasswordModal {...props} />);
+    expect(baseElement).toBeTruthy();
   });
 });

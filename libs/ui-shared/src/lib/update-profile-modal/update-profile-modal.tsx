d78b0e5c5ce57dc09822393a30a1769cc6662f89
m-ibrahim-khalil <mik858692@gmail.com>
2023-08-09 19:36:12 +0600

    fixed minor design issue

--- libs/ui-shared/src/lib/update-profile-modal/update-profile-modal.tsx
+++ libs/ui-shared/src/lib/update-profile-modal/update-profile-modal.tsx
@@ -11,7 +11,7 @@ export function UpdateProfileModal({
   user,
 }: UpdateProfileModalProps) {
   return (
-    <div className="fixed z-50 w-full overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full bg-gray-600 bg-opacity-50">
+    <div className="fixed z-50 w-full overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full backdrop-blur-lg bg-transparent">
       <div className="relative top-20 mx-auto w-full max-w-lg max-h-full">
         <UpdateProfileForm setShowModal={setShowModal} user={user} />
       </div>
--- libs/ui-shared/src/lib/update-user-password-modal/update-user-password-modal.tsx
+++ libs/ui-shared/src/lib/update-user-password-modal/update-user-password-modal.tsx
@@ -32,7 +32,7 @@ export function UpdateUserPasswordModal({
     } else setError(res ?? updatePasswordError?.message ?? 'Unknown error');
   };
   return (
-    <div className="fixed z-50 w-full overflow-y-auto md:inset-0 h-[calc(100%-1rem)]  max-h-full bg-gray-600 bg-opacity-50">
+    <div className="fixed z-50 w-full overflow-y-auto md:inset-0 h-[calc(100%-1rem)]  max-h-full backdrop-blur-lg bg-transparent">
       <div className="relative top-20 mx-auto w-full  max-w-md max-h-full">
         <UpdateUserPasswordForm
           setShowModal={setShowModal}
